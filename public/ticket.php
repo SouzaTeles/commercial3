e33d136ec3b0f98c8e7409252313e8a94aada906
Unknown <alessandro@futuraagencia.com.br>
2018-09-26 09:05:21 -0300

    Melhorias

--- public/ticket.php
+++ public/ticket.php
@@ -1,5 +1,9 @@
 <?php
 
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, X-Requested-With, x-session-token, System-Version');
+
     include "../config/start.php";
 
     GLOBAL $commercial, $post;
@@ -10,7 +14,6 @@
     $ticket_id = Model::insert($commercial,(Object)[
         "table" => "Ticket",
         "fields" => [
-            [ "person_id", "s", $post->person->IdPessoa ],
             [ "company_id", "i", $post->data->company_id ],
             [ "ticket_type_id", "i", $post->data->ticket_type_id ],
             [ "urgency_id", "i", $post->data->urgency_id ],
@@ -22,13 +25,13 @@
 
     $ticket_code = substr("00000{$ticket_id}",-6);
 
-    $post->data->message = "<p>{$post->data->CdChamada} - {$post->data->NmPessoa}</p>" . $post->data->message;
+    $post->data->message = "<p>{$post->person->CdChamada} - {$post->person->NmPessoa}</p>" . $post->data->message;
     $ticket_note_id = Model::insert($commercial,(Object)[
         "table" => "TicketNote",
         "fields" => [
             [ "ticket_id", "i", $ticket_id ],
             [ "ticket_note_text", "s", removeSpecialChar($post->data->message) ],
-            [ "ticket_date", "s", date("Y-m-d H:i:s") ]
+            [ "ticket_note_date", "s", date("Y-m-d H:i:s") ]
         ]
     ]);
 
@@ -37,6 +40,7 @@
         "name" => "Ti Dafel"
     ];
 
+    $files = [];
     if( @$post->images ){
         $post->images = (Object)$post->images;
 
@@ -45,7 +49,6 @@
             mkdir($path, 0755, true);
         }
 
-        $files = [];
         foreach( $post->images as $key => $image ){
             $fileName = date("YmdHis") + ($key+1);
             $data = explode( ',', $image );
@@ -55,7 +58,7 @@
             fwrite( $ifp, base64_decode( $data[ 1 ] ) );
             fclose( $ifp );
 
-            $files[] = "{$path}/{$fileName}.{$extension}";
+            $files[] = "{$fileName}.{$extension}";
 
             Model::insert($commercial,(Object)[
                 "table" => "TicketFile",
@@ -66,19 +69,25 @@
                 ]
             ]);
         }
-
-        email((Object)[
-            "origin" => "ticket",
-            "parent_id" => $ticket_id,
-            "subject" => "Chamado {$ticket_code}",
-            "recipient" => $to,
-            "files" => $files
-        ]);
     }
 
-    echo json_encode((Object)[
+    $ticket = (Object)[
         "ticket_id" => $ticket_id,
         "ticket_code" => $ticket_code
+    ];
+
+    email((Object)[
+        "origin" => "ticket",
+        "parent_id" => $ticket_id,
+        "subject" => "Chamado {$ticket_code}",
+        "recipient" => $to,
+        "files" => $files,
+        "vars" => [(Object)[
+            "key" => "ticket",
+            "data" => $ticket
+        ]]
     ]);
+
+    echo json_encode($ticket);
 
 ?>
