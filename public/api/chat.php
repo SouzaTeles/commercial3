89605e5099b85382f7e11a46d3a431f8a7c377d9
Unknown <alessandro@futuraagencia.com.br>
2018-10-09 18:28:32 -0300

    Chat p2

--- public/api/chat.php
+++ public/api/chat.php
@@ -15,21 +15,56 @@
 
     switch( $get->action )
     {
+        case "add":
+
+            if( !@$post->to_id || !@$post->message_type || !@$post->message_text ){
+                headerResponse((Object)[
+                    "code" => 417,
+                    "message" => "Parâmetro POST não encontrado."
+                ]);
+            }
+
+            $date = date("Y-m-d H:i:s");
+            $message_id = (int)Model::insert($commercial,(Object)[
+                "table" => "[Message]",
+                "fields" => [
+                    ["to_id", "s", $post->to_id],
+                    ["from_id", "s", $login->user_id],
+                    ["message_status", "s", "O"],
+                    ["message_type", "s", $post->message_type],
+                    ["message_text", "s", $post->message_text],
+                    ["message_date", "s", $date],
+                ]
+            ]);
+
+            Json::get($headerStatus[200],(Object)[
+                "message_date" => $date,
+                "message_status" => "O",
+                "message_id" => $message_id,
+                "message_text" => $post->message_text,
+            ]);
+
+        break;
+
         case "getList":
 
             $chat = Model::getList($commercial,(Object)[
                 "tables" => [
-                    "[User]"
+                    "[User] U"
                 ],
                 "fields" => [
-                    "user_id",
-                    "person_id",
-                    "user_name",
-                    "user_timestamp"
+                    "U.user_id",
+                    "U.person_id",
+                    "U.user_name",
+                    "U.user_timestamp",
+                    "message_status=(SELECT TOP 1 M1.message_status FROM [Message] M1 WHERE ((M1.from_id={$login->user_id} AND M1.to_id=U.user_id) OR (M1.to_id={$login->user_id} AND M1.from_id=U.user_id)) ORDER BY M1.message_date DESC)",
+                    "message_text=(SELECT TOP 1 M2.message_text FROM [Message] M2 WHERE ((M2.from_id={$login->user_id} AND M2.to_id=U.user_id) OR (M2.to_id={$login->user_id} AND M2.from_id=U.user_id)) ORDER BY M2.message_date DESC)",
+                    "message_date=(SELECT TOP 1 M3.message_date FROM [Message] M3 WHERE ((M3.from_id={$login->user_id} AND M3.to_id=U.user_id) OR (M3.to_id={$login->user_id} AND M3.from_id=U.user_id)) ORDER BY M3.message_date DESC)",
                 ],
                 "filters" => [
-                    [ "user_id", "i", "!=", $login->user_id ]
-                ]
+                    [ "U.user_id", "i", "!=", $login->user_id ]
+                ],
+                "order" => "(SELECT TOP 1 M4.message_date FROM [Message] M4 WHERE ((M4.from_id={$login->user_id} AND M4.to_id=U.user_id) OR (M4.to_id={$login->user_id} AND M4.from_id=U.user_id)) ORDER BY M4.message_date DESC) DESC"
             ]);
 
             foreach( $chat as $data ){
@@ -47,6 +82,39 @@
             }
 
             Json::get( $headerStatus[200], $chat );
+
+        break;
+
+        case "getMessages":
+
+            if( !@$post->user_id ){
+                headerResponse((Object)[
+                    "code" => 417,
+                    "message" => "Parâmetro GET não encontrado."
+                ]);
+            }
+
+            $messages = Model::getList($commercial,(Object)[
+                "top" => 10,
+                "tables" => [ "[Message]" ],
+                "fields" => [
+                    "message_id",
+                    "to_id",
+                    "from_id",
+                    "message_text",
+                    "message_date",
+                    "message_status"
+                ],
+                "filters" => [
+                    [
+                        ["to_id={$post->user_id} and from_id={$login->user_id}"],
+                        ["to_id={$login->user_id} and from_id={$post->user_id}"]
+                    ]
+                ],
+                "order" => "message_date desc"
+            ]);
+
+            Json::get($headerStatus[200],$messages);
 
         break;
     }
