c0bc458dd7cc4061bf0969ec1b2288e28add50a8
Unknown <alessandro@futuraagencia.com.br>
2018-10-11 10:59:42 -0300

    3.1.9
    - Aniversariantes

--- public/css/home.css.php
+++ public/css/home.css.php
@@ -1,6 +1,6 @@
 <?php include "data.css.php"; ?>
 
-.slide
+#slide
 {
     width: 100%;
     height: 276px;
@@ -12,7 +12,7 @@
     border-bottom: 6px solid <?php echo $colors->hex->palette["blue"]; ?>;
 }
 
-.slide .image
+#slide .image
 {
     height: 270px;
     background-size: cover;
@@ -20,29 +20,29 @@
     background-repeat: no-repeat;
 }
 
-.slide .carousel-caption h3
+#slide .carousel-caption h3
 {
     font-size: 56px;
     padding: 4px 10px;
     display: inline-block;
 }
 
-.slide .carousel-caption p
+#slide .carousel-caption p
 {
     font-size: 22px;
 }
 
-.slide .carousel-caption button
+#slide .carousel-caption button
 {
     padding: 10px;
 }
 
-.slide .carousel-caption button a
+#slide .carousel-caption button a
 {
     font-size: 16px;
 }
 
-.slide ol
+#slide ol
 {
     margin: 0;
     padding: 0;
@@ -50,8 +50,8 @@
     width: 100%;
 }
 
-.slide ol li,
-.slide ol li.active
+#slide ol li,
+#slide ol li.active
 {
     width: 20px;
     height: 6px;
@@ -59,8 +59,69 @@
     border-radius: 0;
 }
 
-.slide ol li:hover,
-.slide ol li.active
+#slide ol li:hover,
+#slide ol li.active
 {
     background-color: <?php echo $colors->hex->palette["blue"]; ?>;
+}
+
+#birthdays
+{
+    width: 100%;
+    height: 300px;
+    margin-top: 20px;
+    text-align: center;
+    position: relative;
+    padding: 56px 20px 20px;
+    background-size: contain;
+    background-color: #efefef;
+    background-repeat: no-repeat;
+    background-position: center top;
+    border-bottom: 6px solid <?php echo $colors->hex->palette["green"]; ?>;
+    border-bottom: 6px solid <?php echo $colors->hex->palette["green"]; ?>;
+    background-image: url('https://wwwcdn.wthr.com/sites/wthr.com/files/styles/article_image/public/Balloon970.jpg?itok=mxndp96r');
+}
+
+#birthdays .image
+{
+    width: 80px;
+    height: 80px;
+    margin: 0 auto;
+    border-radius: 50%;
+    margin-bottom: 10px;
+    background-size: cover;
+    background-color: #efefef;
+    background-position: center;
+    background-image: url('../images/empty-image.png');
+}
+
+#birthdays .name
+{
+    font-size: 18px;
+}
+
+#birthdays .date
+{
+    font-size: 26px;
+}
+
+#birthdays .text
+{
+    line-height: 16px;
+}
+
+#birthdays a
+{
+    bottom: 6px;
+    position: absolute;
+}
+
+#birthdays a.left
+{
+    left: 110px;
+}
+
+#birthdays a.right
+{
+    right: 110px;
 }
